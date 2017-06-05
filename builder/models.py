d360a952d8f18711f8da0e9683b761ab7fa598b1
roushan kumar <roushan.101004@gmail.com>
2017-06-05 15:04:06 +0530

    changed the view

--- builder/models.py
+++ builder/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Data(models.Model):
+    path = models.CharField(max_length=200)
+    userid = models.CharField(max_length=20)
+    passwd = models.CharField(max_length=50)
+
--- builder/views.py
+++ builder/views.py
@@ -1,37 +1,48 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.http import HttpResponse,HttpRequest
 from django.views.decorators.csrf import csrf_exempt
 import json
 import os
 import py_compile
 import logging
+from . import models
 logging.basicConfig(format='%(asctime)s %(message)s',filename='Events.log',level=logging.INFO)
 from django.shortcuts import render
 import subprocess
 from subprocess import check_output
+import urllib2
+import requests
+import requests.packages.urllib3
+requests.packages.urllib3.disable_warnings()
 # Create your views here.
 @csrf_exempt
 def home(request):
     body_data = {'foo' : "bar"}
     if request.method == "POST":
-        if 'push'  not in request.body: #not a payload
-            if not (os.path.exists(request.POST['path'])):
-                return render(request, 'index.html',{'req':' ','error':'Invalid inputs.Please enter correct inputs'})
-            request.session['path'] = request.POST['path']
-            request.session['userid'] = request.POST['userid']
-            request.session['passwd'] = request.POST['passwd']
-            return render(request, 'index.html')
+        if 'pullrequest'  not in request.body: #not a payload
+            if 'path' in request.POST:
+                if not (os.path.exists(request.POST['path'])):
+                    return render(request, 'index.html',{'req':' ','error':'Invalid inputs.Please enter correct inputs'})
+                data = models.Data()
+                data.path = request.POST['path']
+                data.userid = request.POST['userid']
+                data.passwd = request.POST['passwd']
+                data.save()
+                return render(request, 'index.html')
+            else:
+                return render(request, 'index.html')
 
         else: #payload
             logging.info('Recieved a Payload')
             #body_unicode = request.body.decode('utf-8')
             body_data = json.loads(request.body)
             if body_data.has_key('pullrequest'):
+                print body_data
                 logging.info('Pull request payload') #send to log
                 link= body_data['pullrequest']['source']['repository']['links']['html']['href']
                 link = link+ '/branch/master'
-                os.chdir(request.session['path'])
+                data = models.Data.objects.get(pk =1)
+                os.chdir(data.path)
                 subprocess.call(['git', 'fetch',link])
                 logging.info('Fetched the repo')
                 output = check_output(['git', 'diff', '--name-only', 'master', 'FETCH_HEAD']).split()
@@ -39,7 +50,9 @@ def home(request):
                 if compile_output(output): #no syntax errors, Merge the pr
                     try:
                         merge_link = body_data["pullrequest"]['links']["merge"]['href']
-                        r= requests.post(merge_link, auth=(request.session['userid'], request.session['passwd']))
+                        userid = data.userid
+                        passwd = data.passwd
+                        r = requests.post(merge_link, auth=('roushankr05', 'reshu5618371'))
                         if r.status_code == 200:#merged successfully
                             tagname = body_data["pullrequest"]['links']["html"]['href']
                             tagname = 'pr'+tagname[len(tagname)-1:]
@@ -48,21 +61,21 @@ def home(request):
                             logging.info('created a tag')
                         logging.info(r.status_code)
                         logging.info(r.content)
-                    except Exception:
+                    except Exception as e:
+                        logging.info(e)
                         logging.info("failed to connect")
-                        logging.info(r.status_code)
-                        logging.info(r.content)
 
                 else: #found syntax errors, decline the pr
                     try:
                         decline_link = body_data["pullrequest"]['links']["decline"]['href']
-                        requests.post(decline_link, auth=(request.session['userid'], request.session['passwd']))
+                        r= requests.post(decline_link, auth=(data.userid, data.passwd))
+                        logging.info('Declined pull request')
                         logging.info(r.status_code)
                         logging.info(r.content)
-                    except Exception:
+                    except Exception as e:
                         logging.info("failed to connect")
-                        logging.info(r.status_code)
-                        logging.info(r.content)
+                        logging.info(e)
+
             return render(request, 'index.html',{'req':body_data})
     else:
         return render(request,'index.html', {'req':"req"})
